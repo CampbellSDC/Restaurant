6b4cf630f5a4456988d96fb711d7ab2ab37ca7a9
Steve <scampbellatc@gmail.com>
2023-03-09 05:57:43 -0500

    updated JS functionality

--- index.js
+++ index.js
@@ -1,66 +1,85 @@
 import { menuArray } from "./data.js";
 
 const menuItemContainer = document.getElementById('hero-container')
-const addItemBtn = document.getElementById('add-btn')
-const yourOrderSection = document.getElementById('your-order')
+
+
+const checkoutItems = document.getElementById('your-order')
+const itemNames = document.getElementById('ordered-items')
 
 let itemsOrdered = []
 
-document.addEventListener('click', function(e){
-   if(e.target.dataset.button === 'Pizza'){
-       handlePizzaClick(e.target.dataset.button)
-   }
-   else if(e.target.dataset.button === 'Hamburger'){
-       handleHamburgerClick(e.target.dataset.button)
-   }
-   else if(e.target.dataset.button === 'Beer'){
-       handleBeerClick(e.target.dataset.button)
-   }
 
+
+document.addEventListener('click', function(e){
+    
+   if(e.target.dataset.button){
+        newOrderItems(e.target.dataset.button)
+    }
+    
+  
 })
 
 
+function newOrderItems(itemId) {
+    const targetItem = menuArray.filter((item)=>{
+        
 
-function handlePizzaClick(pizzas){
+       return item.id == itemId
+    // if(item.name === itemId){
+    //     console.log(item)
+
     
-    menuArray.forEach((element) => {
-        if(element.name === pizzas){
-            itemsOrdered.push(element)
-            
-        }
-        
-    }
-    )
-
-    console.log(itemsOrdered)}
-
-function handleHamburgerClick(hamburgers){
+    })[0]
     
-    menuArray.forEach((element) => {
-        
-        if(element.name === hamburgers){
-            itemsOrdered.push(element)
-
-        }
-        
-    })
+    itemsOrdered.push(targetItem)
     console.log(itemsOrdered)
+    renderOrder()
+    
 }
 
-function handleBeerClick(beers){
-    menuArray.forEach((element) => {
-        if(element.name === beers){
-            itemsOrdered.push(element)
-        }
+
+function renderOrder(){
+    
+    let orderHtml = ''
+   
+    itemsOrdered.forEach(({name, price}) => {
+        
+       
+        orderHtml += `
+        <div id="order-line-item" >
+        <div id='item'>
+            <h2>${name}</h2>
+            <button class="remove-btn">remove</button>
+        </div>
+
+        <h3>$${price}</h3>
+        </div>
+        `
     })
-    console.log(itemsOrdered)
+    
+   checkoutItems.classList.remove('hidden')
+    itemNames.innerHTML = orderHtml
+    getTotalPrice()
+}   
+
+function getTotalPrice()  {
+    let total = document.getElementById('total')
+    const sum = itemsOrdered.reduce((price, total)=> price + total.price, 0 )
+
+    total.innerHTML =`
+        $${sum}
+    ` 
+
+    
 }
+
+
 
 function render(){
-
+    let itemsHtml = ''
     menuArray.forEach(function(item){
         
-        menuItemContainer.innerHTML += `
+        itemsHtml += `
         <div class="menu-item-container">
 
             <div class="emoji-item-container">
@@ -74,7 +93,7 @@ function render(){
                     <p id="pizza-toppings">${item.ingredients}</p>
                     <h4 id="pizza-price">$${item.price}</h4>     
                 </div>
-                <button class="add-item-btn" data-button="${item.name}">+</button>
+                <button class="add-item-btn" data-button="${item.id}">+</button>
             </div>
 
     
@@ -84,6 +103,7 @@ function render(){
         </div>
     `
     })
+    menuItemContainer.innerHTML = itemsHtml
     
 }
 
