003ceb1204708bd3d2448effad8d006ab9ad9d98
Steve <scampbellatc@gmail.com>
2023-03-12 18:02:38 -0400

    modal and payment buttons

--- index.js
+++ index.js
@@ -1,176 +1,171 @@
 import { menuArray } from "./data.js";
 
-const menuItemContainer = document.getElementById('hero-container')
+const menuItemContainer = document.getElementById("hero-container");
 
+const checkoutItems = document.getElementById("your-order");
+const itemNames = document.getElementById("ordered-items");
+const modal = document.getElementById('modal-container')
+const payBtn = document.getElementById('payBtn')
+const name = document.getElementById('name')
 
-const checkoutItems = document.getElementById('your-order')
-const itemNames = document.getElementById('ordered-items')
+const orderQuantity = {};
 
+let itemsOrdered = [];
 
-
-const orderQuantity = {}
-
-let itemsOrdered = []
-
-
-
-document.addEventListener('click', function(e){
-    
-   if(e.target.dataset.button === '0' || e.target.dataset.button === '1' || e.target.dataset.button === '2'){
-        newOrderItems(e.target.dataset.button)
-    }
-    else if(e.target.dataset.button === "removeBtn") {
-        const itemId = e.target.closest('#order-line-item').dataset.id
-        removeBtn(itemId)
-    }
-    else if(e.target.dataset.button === "completeOrderBtn"){
-        completeOrder()
-    }
-    
-  
-})
+document.addEventListener("click", function (e) {
+  if (
+    e.target.dataset.button === "0" ||
+    e.target.dataset.button === "1" ||
+    e.target.dataset.button === "2"
+  ) {
+    newOrderItems(e.target.dataset.button);
+  } else if (e.target.dataset.button === "removeBtn") {
+    const itemId = e.target.closest("#order-line-item").dataset.id;
+    removeBtn(itemId);
+  } else if (e.target.dataset.button === "completeOrderBtn") {
+    completeOrder();
+  }
+});
 
 // COMPLETE ORDER BUTTON FUNCTION
 
 function completeOrder() {
-    console.log("You've completed your order")
+  modal.classList.remove('hidden')
 }
 
+
+payBtn.addEventListener('click', function(){
+    modal.classList.add('hidden')
+    const fullName = name.value
+    itemNames.classList.add('hidden')
+    let thankYouNote = ''
+
+    thankYouNote = `
+    <div class='thank-you-note'>
+
+        <p>Thanks, ${fullName}, your order is on it's way! </p>
+
+    </div>
+    `
+
+    checkoutItems.innerHTML = thankYouNote
+    
+
+
+})
 
 //ITEMS ADDED TO ORDER
 function newOrderItems(itemId) {
-    const targetItem = menuArray.filter((item)=>{
-        return item.id == itemId
-    })[0]
+  const targetItem = menuArray.filter((item) => {
+    return item.id == itemId;
+  })[0];
 
-    if(itemsOrdered.includes(targetItem)){
-        //If order basket already contains the selected item, no need to add to basket just increment the quantity
+  if (itemsOrdered.includes(targetItem)) {
+    //If order basket already contains the selected item, no need to add to basket just increment the quantity
 
-        // remove button will need to decrement this count and update totals
-            orderQuantity[targetItem.name]+=1;
-        }else{
-         //If order basket does not contain the selected item then set quantity to one and add to basket
-            orderQuantity[targetItem.name]=1;
-            itemsOrdered.push(targetItem)
-        }
-    
-    
-    renderOrder()
-    console.log(orderQuantity)
+    // remove button will need to decrement this count and update totals
+    orderQuantity[targetItem.name] += 1;
+  } else {
+    //If order basket does not contain the selected item then set quantity to one and add to basket
+    orderQuantity[targetItem.name] = 1;
+    itemsOrdered.push(targetItem);
+  }
+
+  renderOrder();
+  console.log(orderQuantity);
 }
 
 
+// RENDER ORDERS
 
+function renderOrder() {
+  let orderHtml = "";
+  let multipleItemHtml = "";
+  const item = document.getElementById("item");
+  let total = 0;
 
-function renderOrder(){
-    
-    let orderHtml = ''
-    let multipleItemHtml = ''
-    const item = document.getElementById('item')
-    let total = 0
-    
-    itemsOrdered.forEach(({name, price, id}) => {
-        const quantity = orderQuantity[name]
-        let itemTotalPrice = price * orderQuantity[name]
-        total += itemTotalPrice
-        orderHtml += `
+  itemsOrdered.forEach(({ name, price, id }) => {
+    const quantity = orderQuantity[name];
+    let itemTotalPrice = price * orderQuantity[name];
+    total += itemTotalPrice;
+    orderHtml += `
         <div id="order-line-item" data-id="${id}" >
             <div id='item'>
                 <h2>${name}</h2>
                 <button class="remove-btn" data-button="removeBtn">remove</button>
-                ${quantity > 1 ? `<h3 class="multiple-items"> X ${quantity}</h3>` : ''}
+                ${
+                  quantity > 1
+                    ? `<h3 class="multiple-items"> X ${quantity}</h3>`
+                    : ""
+                }
                
             </div>
     
             <h3>$${itemTotalPrice}</h3>
         </div>
-        `
-    })
-        
-    checkoutItems.classList.remove('hidden')
-    itemNames.innerHTML = orderHtml
-    getTotalPrice()
-   
+        `;
+  });
+
+  checkoutItems.classList.remove("hidden");
+  itemNames.innerHTML = orderHtml;
+  getTotalPrice();
 }
 
+//GET TOTAL PRICE FOR ORDERS
 
+function getTotalPrice() {
+  let total = 0;
+  let itemTotal = document.getElementById("item");
 
+  for (let i = 0; i < itemsOrdered.length; i++) {
+    for (let j = 0; j < Object.keys(orderQuantity).length; j++) {
+      if (itemsOrdered[i].name === Object.keys(orderQuantity)[j]) {
+        let sumTotal = itemsOrdered[i].price * Object.values(orderQuantity)[j];
+        total += sumTotal;
+        const priceElements = document.querySelectorAll(
+          `[data-name="${itemsOrdered[i].name}"] [data-price="${itemsOrdered[i].price}"]`
+        );
 
-
-
-function getTotalPrice()  {
-
-    let total = 0
-    let itemTotal = document.getElementById('item')
-
-    for(let i = 0; i<itemsOrdered.length; i++){
-
-        for(let j =0; j < Object.keys(orderQuantity).length; j++){
-
-            if(itemsOrdered[i].name === Object.keys(orderQuantity)[j]){
-
-                let sumTotal = itemsOrdered[i].price * Object.values(orderQuantity)[j]
-                total += sumTotal
-                const priceElements = document.querySelectorAll(`[data-name="${itemsOrdered[i].name}"] [data-price="${itemsOrdered[i].price}"]`)
-
-
-                priceElements.forEach(element => {
-                    element.textContent = `
+        priceElements.forEach((element) => {
+          element.textContent = `
                         $${sumTotal}
-                    `
-                })
-            }
-        }
+                    `;
+        });
+      }
     }
-   
-   const totalElement = document.getElementById('total')
-   totalElement.textContent =  `
+  }
+
+  const totalElement = document.getElementById("total");
+  totalElement.textContent = `
     $${total}
-   `
-    
-    
+   `;
 }
 
 // REMOVE ITEM FROM ORDER LIST
 
-
-function removeBtn(itemId){
-    
-    
-    /* One issue I had here was that the ItemId parameter was given as a string
+function removeBtn(itemId) {
+  /* One issue I had here was that the ItemId parameter was given as a string
     while the item.id below was returned as a number, and I was trying to return
     two items with strict equality. Changing it to loose equality fixed the issue
     */
-    const targetItem = menuArray.filter((item)=>{
-        return item.id == itemId
-        
-    })[0]
+  const targetItem = menuArray.filter((item) => {
+    return item.id == itemId;
+  })[0];
 
-    
+  if (itemsOrdered.includes(targetItem) >= 0) {
+    orderQuantity[targetItem.name]--;
 
-    if(itemsOrdered.includes(targetItem) >= 0){
-        
-        orderQuantity[targetItem.name]--
-
-        if(orderQuantity[targetItem.name] === 0){
-            itemsOrdered = itemsOrdered.filter(item => item !== targetItem)
-        }
-        renderOrder()
-        
-        
+    if (orderQuantity[targetItem.name] === 0) {
+      itemsOrdered = itemsOrdered.filter((item) => item !== targetItem);
     }
-   
-
-    
+    renderOrder();
+  }
 }
 
-
-
-function render(){
-    let itemsHtml = ''
-    menuArray.forEach(function(item){
-        
-        itemsHtml += `
+function render() {
+  let itemsHtml = "";
+  menuArray.forEach(function (item) {
+    itemsHtml += `
         <div class="menu-item-container">
 
             <div class="emoji-item-container">
@@ -192,22 +187,9 @@ function render(){
                 
 
         </div>
-    `
-    })
-    menuItemContainer.innerHTML = itemsHtml
-    
+    `;
+  });
+  menuItemContainer.innerHTML = itemsHtml;
 }
 
-
-
-
-
-render()
-
-
-
-
-
-
-
-
+render();
